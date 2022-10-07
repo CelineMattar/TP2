61e1673be0385e7d2b9dfe1c58db6bf81d470a93
mattarc <mattarc@educ.college-montreal.qc.ca>
2022-10-07 16:13:01 -0400

    explications

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ boucle_jeu = True
 while boucle_jeu:
     import random
     chiffre_random = random.randint (1,15)
+    print("Dans ce jeu vous devriez deviner un chiffre entre 1 et 15. Si vous avez la mauvaises reponse, vous auriez un indice.")
     reponse = int(input("Ecrivez un chiffre entre 1 et 15"))
     if reponse == chiffre_random:
         print("yay")
     if reponse > chiffre_random :
         print ("essaye un chiffre plus petit")
-    if reponse < chiffre random :
+    if reponse < chiffre_random :
         print ("esaye un chiffre plus grand")
